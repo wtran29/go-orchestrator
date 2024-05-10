45e81a5b5bfdf68c2cf18daca0f2760b782420f8
William Tran <wtran29@gmail.com>
2024-05-10 10:28:48 -0700

    clean up comment

--- task/task.go
+++ task/task.go
@@ -17,14 +17,14 @@ const (
 	Failed
 )
 
-// Task represents a task that auser wants to run on cluster
+// Task represents a task that a user wants to run on cluster
 type Task struct {
 	ID            uuid.UUID
 	Name          string
 	State         State
 	Image         string // what docker image task should use
-	Memory        int // amount of memory needed
-	Disk          int // amount of disk space needed
+	Memory        int    // amount of memory needed
+	Disk          int    // amount of disk space needed
 	ExposedPorts  nat.PortSet
 	PortBindings  map[string]string
 	RestartPolicy string
@@ -35,8 +35,8 @@ type Task struct {
 // TaskEvent represents an even that moves a Task from
 // one state to another
 type TaskEvent struct {
-	ID uuid.UUID
-	State State
+	ID        uuid.UUID
+	State     State
 	Timestamp time.Time
-	Task Task
+	Task      Task
 }
