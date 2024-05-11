881941cd74db7c0e00ba7ba17b5960f2c4dbd871
William Tran <wtran29@gmail.com>
2024-05-10 21:35:50 -0700

    add createContainer and stopContainer functions

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/docker/docker/client"
 	"github.com/golang-collections/collections/queue"
 	"github.com/google/uuid"
 	"github.com/wtran29/go-orchestrator/manager"
@@ -61,4 +62,36 @@ func main() {
 		Role:   "worker",
 	}
 	fmt.Printf("node: %v\n", n)
+}
+
+func createContainer() (*task.Docker, *task.DockerResult) {
+	c := task.Config{
+		Name:  "test-container-1",
+		Image: "postgres:14",
+		Env: []string{
+			"POSTGRES_USER=archon",
+			"POSTGRES_PASSWORD=secret",
+		},
+	}
+	dc, _ := client.NewClientWithOpts(client.FromEnv)
+	d := task.Docker{
+		Client: dc,
+		Config: c,
+	}
+	result := d.Run()
+	if result.Error != nil {
+		fmt.Printf("%v\n", result.Error)
+		return nil, nil
+	}
+	fmt.Printf("Container %s is running with config %v\n", result.ContainerId, c)
+	return &d, &result
+}
+
+func stopContainer(d *task.Docker, id string) *task.DockerResult {
+	result := d.Stop(id)
+	if result.Error != nil {
+		fmt.Printf("%v\n", result.Error)
+	}
+	fmt.Printf("Container %s has been stopped and removed\n", result.ContainerId)
+	return &result
 }
