c8180ef3516fd6fdf570756c2e57c7c9ce6d666e
William Tran <wtran29@gmail.com>
2024-05-11 00:00:01 -0700

    add logic to add task, start task and stop task

--- worker/worker.go
+++ worker/worker.go
@@ -2,6 +2,8 @@ package worker
 
 import (
 	"fmt"
+	"log"
+	"time"
 
 	"github.com/golang-collections/collections/queue"
 	"github.com/google/uuid"
@@ -11,9 +13,9 @@ import (
 // Worker runs and keep tracks of all the tasks
 type Worker struct {
 	Name      string
-	Queue     queue.Queue             // tasks handled in (FIFO)
-	Db        map[uuid.UUID]task.Task // to keep track of tasks
-	TaskCount int                     //keep track of number of tasks as worker
+	Queue     queue.Queue              // tasks handled in (FIFO)
+	Db        map[uuid.UUID]*task.Task // to keep track of tasks
+	TaskCount int                      //keep track of number of tasks as worker
 }
 
 // RunTask handles running a task on the machine where worker is running
@@ -21,14 +23,42 @@ func (w *Worker) RunTask() {
 	fmt.Println("start or stop a task")
 }
 
+func (w *Worker) AddTask(t task.Task) {
+	w.Queue.Enqueue(t)
+}
+
 // StartTask starts a task
-func (w *Worker) StartTask() {
-	fmt.Println("start a task")
+func (w *Worker) StartTask(t task.Task) task.DockerResult {
+	t.StartTime = time.Now().UTC()
+	config := task.NewConfig(&t)
+	d := task.NewDocker(config)
+	result := d.Run()
+	if result.Error != nil {
+		log.Printf("Error running task %v: %v\n", t.ID, result.Error)
+		t.State = task.Failed
+		w.Db[t.ID] = &t
+		return result
+	}
+	t.ContainerID = result.ContainerId
+	t.State = task.Running
+	w.Db[t.ID] = &t
+	return result
 }
 
 // StopTask stops a task
-func (w *Worker) StopTask() {
-	fmt.Println("stop a task")
+func (w *Worker) StopTask(t task.Task) task.DockerResult {
+	config := task.NewConfig(&t)
+	d := task.NewDocker(config)
+
+	result := d.Stop(t.ContainerID)
+	if result.Error != nil {
+		log.Printf("Error stopping container %v: %v\n", t.ContainerID, result.Error)
+	}
+	t.FinishTime = time.Now().UTC()
+	t.State = task.Completed
+	w.Db[t.ID] = &t
+	log.Printf("Stopped and removed container %v for task %v\n", t.ContainerID, t.ID)
+	return result
 }
 
 // CollectStats periodically collect stats about the worker
