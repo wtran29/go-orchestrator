d84db593fe29c57da3885328840e8acc1dc9213a
William Tran <wtran29@gmail.com>
2024-05-12 13:06:39 -0700

    added send work, update tasks, select worker and add task functions

--- manager/manager.go
+++ manager/manager.go
@@ -1,36 +1,134 @@
 package manager
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
 
 	"github.com/golang-collections/collections/queue"
 	"github.com/google/uuid"
 	"github.com/wtran29/go-orchestrator/task"
+	"github.com/wtran29/go-orchestrator/worker"
 )
 
 // Manager will keep track of the workers in the cluster
 type Manager struct {
 	Pending       queue.Queue // which tasks will be placed upon first being submitted
-	TaskDb        map[string][]task.Task
-	EventDb       map[string][]task.TaskEvent
+	TaskDb        map[uuid.UUID]*task.Task
+	EventDb       map[uuid.UUID]*task.TaskEvent
 	Workers       []string // keep track of the workers
 	WorkerTaskMap map[string][]uuid.UUID
 	TaskWorkerMap map[uuid.UUID]string
+	LastWorker    int
 }
 
 // SelectWorker is responsible for look at requirements specified in a Task
 // and evaluating the resources available in pool of workers to pick the right
 // worker for the task
-func (m *Manager) SelectWorker() {
-	fmt.Println("select worker")
+func (m *Manager) SelectWorker() string {
+	var newWorker int
+	if m.LastWorker+1 < len(m.Workers) {
+		newWorker = m.LastWorker + 1
+		m.LastWorker++
+	} else {
+		newWorker = 0
+		m.LastWorker = 0
+	}
+	return m.Workers[newWorker]
 }
 
 // UpdateTasks will track tasks, their states, and machine on which they run
 func (m *Manager) UpdateTasks() {
-	fmt.Println("update tasks")
+	for _, worker := range m.Workers {
+		log.Printf("Checking worker %v for task updates", worker)
+		url := fmt.Sprintf("http://%s/tasks", worker)
+		resp, err := http.Get(url)
+		if err != nil {
+			log.Printf("Error connecting to %v: %v\n", worker, err)
+		}
+		if resp.StatusCode != http.StatusOK {
+			log.Printf("Error sending request: %v\n", err)
+		}
+
+		d := json.NewDecoder(resp.Body)
+		var tasks []*task.Task
+		err = d.Decode(&tasks)
+		if err != nil {
+			log.Printf("Error unmarshalling tasks: %s\n", err.Error())
+
+		}
+		for _, t := range tasks {
+			log.Printf("Attempting to update task %v\n", t.ID)
+
+			_, ok := m.TaskDb[t.ID]
+			if !ok {
+				log.Printf("Task with ID %s not found\n", t.ID)
+				return
+			}
+			if m.TaskDb[t.ID].State != t.State {
+				m.TaskDb[t.ID].State = t.State
+			}
+
+			m.TaskDb[t.ID].StartTime = t.StartTime
+			m.TaskDb[t.ID].FinishTime = t.FinishTime
+			m.TaskDb[t.ID].ContainerID = t.ContainerID
+		}
+	}
 }
 
 // SendWork sends tasks to workers
 func (m *Manager) SendWork() {
-	fmt.Println("send work to workers")
+	if m.Pending.Len() > 0 {
+		w := m.SelectWorker()
+		e := m.Pending.Dequeue()
+		te := e.(task.TaskEvent)
+		t := te.Task
+		log.Printf("Pulled %v off pending queue", t)
+
+		m.EventDb[te.ID] = &te
+		m.WorkerTaskMap[w] = append(m.WorkerTaskMap[w], te.Task.ID)
+		m.TaskWorkerMap[t.ID] = w
+
+		t.State = task.Scheduled
+		m.TaskDb[t.ID] = &t
+
+		data, err := json.Marshal(te)
+		if err != nil {
+			log.Printf("Unable to Marshal task object: %v\n", t)
+		}
+		url := fmt.Sprintf("http://%s/tasks", w)
+		resp, err := http.Post(url, "application/json", bytes.NewBuffer(data))
+		if err != nil {
+			log.Printf("Error connecting to %v: %v", w, err)
+			m.Pending.Enqueue(t)
+			return
+		}
+		d := json.NewDecoder(resp.Body)
+		if resp.StatusCode != http.StatusCreated {
+			e := worker.ErrResponse{}
+			err := d.Decode(&e)
+			if err != nil {
+				fmt.Printf("Error decoding response: %s\n", err.Error())
+				return
+			}
+			log.Printf("Response error (%d): %s\n", e.HTTPStatusCode, e.Message)
+			return
+		}
+		t = task.Task{}
+		err = d.Decode(&t)
+		if err != nil {
+			fmt.Printf("Error decoding response: %s\n", err.Error())
+			return
+		}
+		log.Printf("%#v\n", t)
+	} else {
+		log.Println("No work in the queue")
+	}
+}
+
+// AddTask adds task to the manager's queue of pending tasks
+func (m *Manager) AddTask(te task.TaskEvent) {
+	m.Pending.Enqueue(te)
 }
