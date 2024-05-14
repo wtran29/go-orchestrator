dbdd83900ea1106d71b337abf968fe466bdd5083
William Tran <wtran29@gmail.com>
2024-05-13 18:07:00 -0700

    add round robin struct, create functions to select candidate nodes, score and pick best node

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -1,10 +1,61 @@
 package scheduler
 
+import (
+	"github.com/wtran29/go-orchestrator/node"
+	"github.com/wtran29/go-orchestrator/task"
+)
+
 // Scheduler determines a set of candidate workers on which task it could run,
 // score the candidate workers from best to worst, and pick the worker with best
 // score
 type Scheduler interface {
-	SelectCandidateNodes()
-	Score()
-	Pick()
+	SelectCandidateNodes(t task.Task, nodes []*node.Node) []*node.Node
+	Score(t task.Task, nodes []*node.Node) map[string]float64
+	Pick(scores map[string]float64, candidates []*node.Node) *node.Node
+}
+
+type RoundRobin struct {
+	Name       string
+	LastWorker int
+}
+
+func (r *RoundRobin) SelectCandidateNodes(t task.Task, nodes []*node.Node) []*node.Node {
+	return nodes
+}
+
+func (r *RoundRobin) Score(t task.Task, nodes []*node.Node) map[string]float64 {
+	nodeScores := make(map[string]float64)
+	var newWorker int
+	if r.LastWorker+1 < len(nodes) {
+		r.LastWorker++
+	} else {
+		newWorker = 0
+		r.LastWorker = 0
+	}
+
+	for idx, node := range nodes {
+		if idx == newWorker {
+			nodeScores[node.Name] = 0.1
+		} else {
+			nodeScores[node.Name] = 1.0
+		}
+	}
+	return nodeScores
+}
+
+func (r *RoundRobin) Pick(scores map[string]float64, candidates []*node.Node) *node.Node {
+	var bestNode *node.Node
+	var lowestScore float64
+	for idx, node := range candidates {
+		if idx == 0 {
+			bestNode = node
+			lowestScore = scores[node.Name]
+			continue
+		}
+		if scores[node.Name] < lowestScore {
+			bestNode = node
+			lowestScore = scores[node.Name]
+		}
+	}
+	return bestNode
 }
