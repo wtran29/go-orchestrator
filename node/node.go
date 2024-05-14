7eab7563c1112f91d9f8184450f6d3505e668ec2
William Tran <wtran29@gmail.com>
2024-05-14 14:48:31 -0700

    refactor get stats function to work with epvm algo

--- node/node.go
+++ node/node.go
@@ -1,5 +1,17 @@
 package node
 
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/wtran29/go-orchestrator/stats"
+	"github.com/wtran29/go-orchestrator/utils"
+)
+
 // Node represents a physical machine where the worker
 // and tasks will run
 type Node struct {
@@ -7,14 +19,45 @@ type Node struct {
 	Ip              string
 	Api             string
 	Cores           int
-	Memory          int
-	MemoryAllocated int
-	Disk            int
-	DiskAllocated   int
+	Memory          int64
+	MemoryAllocated int64
+	Disk            int64
+	DiskAllocated   int64
 	Role            string
 	TaskCount       int
+	Stats           stats.Stats
 }
 
 func NewNode(name string, api string, role string) *Node {
 	return &Node{Name: name, Api: api, Role: role}
+}
+
+func (n *Node) GetStats() (*stats.Stats, error) {
+	var resp *http.Response
+	var err error
+	url := fmt.Sprintf("%s/stats", n.Api)
+	resp, err = utils.HTTPWithRetry(http.Get, url)
+	if err != nil {
+		msg := fmt.Sprintf("Unable to connect to %v. Permanent failure.\n", n.Api)
+		log.Println(msg)
+		return nil, errors.New(msg)
+	}
+	if resp.StatusCode != 200 {
+		msg := fmt.Sprintf("Error retrieving stats from %v: %v", n.Api, err)
+		log.Println(msg)
+		return nil, errors.New(msg)
+	}
+	defer resp.Body.Close()
+	body, _ := io.ReadAll(resp.Body)
+	var stats stats.Stats
+	err = json.Unmarshal(body, &stats)
+	if err != nil {
+		msg := fmt.Sprintf("error decoding message while getting stats for node %s", n.Name)
+		log.Println(msg)
+		return nil, errors.New(msg)
+	}
+	n.Memory = int64(stats.MemTotalKb())
+	n.Disk = int64(stats.DiskTotal())
+	n.Stats = stats
+	return &n.Stats, nil
 }
