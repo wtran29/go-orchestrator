af597159e537f9c27a165fda9258938a08a1aff0
William Tran <wtran29@gmail.com>
2024-05-15 23:00:45 -0700

    designed and implemented worker component for cli

---
+++ cmd/worker.go
@@ -0,0 +1,47 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/google/uuid"
+	"github.com/spf13/cobra"
+	"github.com/wtran29/go-orchestrator/worker"
+)
+
+// workerCmd represents the worker command
+var workerCmd = &cobra.Command{
+	Use:   "worker",
+	Short: "Worker command to operate an Archon worker node.",
+	Long: `archon worker command. 
+
+The worker runs tasks and responds to the managers's requests about task state.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		host, _ := cmd.Flags().GetString("host")
+		port, _ := cmd.Flags().GetInt("port")
+		name, _ := cmd.Flags().GetString("name")
+		dbType, _ := cmd.Flags().GetString("dbtype")
+
+		log.Println("Starting worker.")
+		w := worker.New(name, dbType)
+		api := worker.Api{Address: host, Port: port, Worker: w}
+		go w.RunTasks()
+		go w.CollectStats()
+		go w.UpdateTasks()
+		log.Printf("Starting worker API on http://%s:%d", host, port)
+		api.Start()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(workerCmd)
+	workerCmd.Flags().StringP("host", "H", "0.0.0.0", "Hostname or IP address")
+	workerCmd.Flags().IntP("port", "p", 5556, "Port on which to listen")
+
+	workerCmd.Flags().StringP("name", "n", fmt.Sprintf("worker-%s", uuid.New().String()), "Name of the worker")
+	workerCmd.Flags().StringP("dbtype", "d", "memory", "Type of datastore to use for tasks (\"memory\" or \"persistent\")")
+
+}
